5cbcfee1e5190f0ced046cf1b2a2590cb3ab2125
Max <max.iakovliev@gmail.com>
2016-07-19 23:37:28 +0800

    started skip list implementation

---
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/DataStructures/Lists/ISkipList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Lists
+{
+    public interface ISkipList<T> where T : IComparable<T>
+    {
+        void Add(T item);
+        void Delete(T item);
+        bool Find(T item);
+    }
+}
