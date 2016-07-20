c28bdb981566a2bfa2fde42faf36c0e15a7ed0c6
Max <max.iakovliev@gmail.com>
2016-07-20 21:42:58 +0800

    started implementation of SkipList

---
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/DataStructures/Lists/SkipListNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Lists
+{
+    public class SkipListNode<T> where T : IComparable<T>
+    {
+        public int CompareTo(T other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SkipListNode<T>> Nodes { get; set; }
+        public SkipListNode(Func<ILinkedList<T>> createInstance)
+        {
+            Nodes = new List<SkipListNode<T>>();
+        }
+    }
+}
