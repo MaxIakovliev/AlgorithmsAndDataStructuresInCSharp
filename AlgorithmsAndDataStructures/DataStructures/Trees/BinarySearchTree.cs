35066cb332e4bbc23982dbf618a2a6c782bacf90
Max <max.iakovliev@gmail.com>
2016-07-29 22:25:03 +0800

    Started BinarySearchTree implementation

---
+++ AlgorithmsAndDataStructures/DataStructures/Trees/BinarySearchTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O3.DataStructures.Trees
+{
+    /// <summary>
+    /// Binary Search Tree (BST) is a special type of Binary Tree that follows following condition
+    /// 1. Left child node is smaller than its parent Node
+    /// 2. Right child node is greater than its parent Node
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BinarySearchTree<T> : IBinaryTree<T>, IEnumerable<T> where T : IComparable<T>
+    {
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBinaryTreeNode<T> Find(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBinaryTreeNode<T> Find(T item, out IBinaryTreeNode<T> parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/DataStructures/Trees/IBinaryTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O3.DataStructures.Trees
+{
+    public interface IBinaryTree<T> where T : IComparable<T>
+    {
+        int Count { get; }
+        
+        void Clear();
+
+        void Add(T item);
+
+        bool Contains(T item);
+
+        IBinaryTreeNode<T> Find(T item);
+
+        IBinaryTreeNode<T> Find(T item, out IBinaryTreeNode<T> parent);
+
+        bool Remove(T item);
+    }
+}
