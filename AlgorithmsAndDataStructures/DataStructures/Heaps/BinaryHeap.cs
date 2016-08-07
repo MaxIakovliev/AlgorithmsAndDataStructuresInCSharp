c3c74d2ca0db6d9471e8278ffca2b205da6845e3
Max <max.iakovliev@gmail.com>
2016-08-07 17:00:07 +0800

    started binary heap implementation

---
+++ AlgorithmsAndDataStructures/DataStructures/Heaps/BinaryHeap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O3.DataStructures.Heaps
+{
+    public class BinaryHeap<T> : IHeap<T> where T : IComparable<T>
+    {
+        private List<T> data;
+        public BinaryHeap()
+        {
+            data = new List<T>();
+        }
+        public void Add(T item)
+        {
+            data.Add(item);
+        }
+
+        public T PeekHead()
+        {
+            return data[0];
+        }
+
+        public T PopHead()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int GetParent(int idx)
+        {
+            if (idx > 0)
+                return (int)Math.Floor(((double)idx - 1) / 2);
+
+            return int.MinValue;
+        }
+
+        private int GetLeftChild(int idx)
+        {
+            return 2 * idx + 1;
+        }
+
+        private int GetRightChild(int idx)
+        {
+            return 2 * idx + 2;
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/DataStructures/Heaps/IHeap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O3.DataStructures.Heaps
+{
+    public interface IHeap<T> where T :IComparable<T>
+    {
+        void Add(T item);
+        
+        /// <summary>
+        /// return head value (value retain in  heap) 
+        /// </summary>
+        /// <returns>head value</returns>
+        T PeekHead();
+
+        /// <summary>
+        /// return  value from head and remove it from  heap
+        /// </summary>
+        /// <returns>head value</returns>
+        T PopHead();
+
+
+            
+
+    }
+}
